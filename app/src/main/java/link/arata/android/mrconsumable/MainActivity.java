bc7235c8b6b8277448ddbd6fd0b9adde8192bdb8
webarata3 <webarata3@gmail.com>
2016-01-30 13:33:33 +0900

    ライセンス画面の追加

--- app/src/main/java/link/arata/android/mrconsumable/MainActivity.java
+++ app/src/main/java/link/arata/android/mrconsumable/MainActivity.java
@@ -1,5 +1,7 @@
 package link.arata.android.mrconsumable;
 
+import android.app.Dialog;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +10,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -44,7 +48,26 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_license) {
-            return true;
+            final Dialog dialog = new Dialog(this);
+            dialog.setContentView(R.layout.webview_license);
+            dialog.setTitle("オープンソースライセンス");
+            dialog.setCancelable(true);
+            WebView webView = (WebView)dialog.findViewById(R.id.licenseWebView);
+
+            webView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+            webView.setWebViewClient(new WebViewClient() {
+                @Override
+                public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                    super.onPageStarted(view, url, favicon);
+                }
+
+                @Override
+                public void onPageFinished(WebView view, String url) {
+                    super.onPageFinished(view, url);
+                    dialog.show();
+                }
+            });
+            webView.loadUrl("file:///android_asset/license.html");
         }
 
         return super.onOptionsItemSelected(item);
